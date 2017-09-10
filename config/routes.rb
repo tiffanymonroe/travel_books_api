4b86a920f847eb581a7c80a4c0ba190e384c5cec
Jen Schumann <jenniferschumann1@gmail.com>
2017-09-09 20:01:36 -0600

    user and welcome controller then CORS functionality update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  resources :books
   resources :destinations
+
   resources :users do
     collection do
       post '/login', to: 'users#login'
     end
   end
-  resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
