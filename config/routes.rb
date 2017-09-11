f1f341b7c6f2ac3bcbe63dfc0040df31254f633b
Jen Schumann <jenniferschumann1@gmail.com>
2017-09-11 09:45:36 -0600

    updated login user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,32 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :linkers
+  # resources :linkers
   resources :books
   resources :destinations
 
+  # resources :user do
+
+
   resources :users do
     collection do
-      post '/login', to: 'users#login',
-      post '/post', to: 'users#create',
+      post '/login', to: 'users#login'
+        resources :books, only: [:index, :show, :create]
+        resources :destinations, only: [:index, :show, :create]
+      # post '/post', to: 'users#create'
+    end
 
-  resources :user do
-    collection do
-      patch '/users/:id', to: 'users#update',
-      put '/users/:id', to: 'users#update',
-      delete '/users/:id', to: 'users#destroy'
-    end
   end
-    end
   end
-end
+
+
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+
+  # rails routes does not like the following line:
+  # put '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
