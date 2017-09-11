2b512cbcc00ebb2076424555f0ea43abafb29ba8
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-09-10 19:10:04 -0600

    updated user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910233829) do
+ActiveRecord::Schema.define(version: 20170911010852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 20170910233829) do
     t.string "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "book"
+    t.string "destination"
   end
 
   add_foreign_key "linkers", "books"
