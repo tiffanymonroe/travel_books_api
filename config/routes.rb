a4cad2a26c4ff958d47c23435d994816118a3670
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-11 21:04:14 -0700

    fixing merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,20 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # resources :linkers
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+  # commented out for testing, most likely need
+  # resources :books
+  # resources :destinations
+  # commented out end
+
+  resources :books, only: [:index, :show]
+  resources :destinations, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :books, only: [:create, :show, :index]
+    resources :destinations, only: [:create, :show, :index]
+
+    end
+  # end
+
   # resources :books do
   #   collection do
   #     get '/books', to: 'books#index'
@@ -11,28 +23,14 @@ Rails.application.routes.draw do
   # end
 
   # resources :user do
-=======
-  resources :books, only: [:index, :show]
-  resources :destinations, only: [:index, :show]
->>>>>>> 72b08a748bd0256f9396aab4bb2ad933b3c136e2
-=======
-  resources :books, only: [:index, :show]
-  resources :destinations, only: [:index, :show]
->>>>>>> 72b08a748bd0256f9396aab4bb2ad933b3c136e2
-
-  resources :users, only: [:index, :show] do
-    resources :books, only: [:index, :show, :create]
-    resources :destinations, only: [:index, :show, :create]
-  end
 
   resources :users do
     collection do
       post '/login', to: 'users#login'
+        # resources :books, only: [:index, :show, :create]
+        # resources :destinations, only: [:index, :show, :create]
+      # post '/post', to: 'users#create'
     end
+
   end
-end
-
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
