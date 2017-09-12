4d2a61092f5427a58475860ad7c942fa83421568
Jen Schumann <jenniferschumann1@gmail.com>
2017-09-12 00:16:19 -0600

    update and delete user working in postman and code cleaned up

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show]
-  # before_action :authorize_user, except: [:index]
+  before_action :authorize_user, except: [:index]
 
   # GET /books
   def index
