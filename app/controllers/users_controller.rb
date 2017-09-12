6516dd2f55a33c737b336b7580ed4bc84564ad9c
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-09-11 21:28:25 -0600

    can FINALLY see books and destinations inside of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,12 +23,21 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    # render json: @users
+    render json: @users.to_json(include: :books, include: :destinations)
   end
 
   # GET /users/1
   def show
     render json: @user
+
+
+    # user_books = @user.books
+    # user_destinations = @user.destinations
+    # render json: { user: @user, books: user_books, destinations: user_destinations }
+    # render json: @user.to_json(include: :books)
+
+
     #in markdown this change is referenced w/same results as commented above:
     # render json: get_current_user
   end
